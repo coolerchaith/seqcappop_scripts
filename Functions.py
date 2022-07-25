4eb14d4d02e469afe0d44795010c73ae2c4f95b8
coolerchaith <matthewhealy97@gmail.com>
2022-07-25 16:50:17 -0400

    Added a timing option and a logging option

--- Functions.py
+++ Functions.py
@@ -63,7 +63,8 @@ def getArgs():
     )
     parser.add_argument(
         '--Log',
-        action="Optional, prints the output of each step in a log file"
+        action="store_true",
+        help="Optional, prints the output of each step in a log file"
     )
     args = parser.parse_args()
     if not args.ReferenceAssembly or not args.MappingFolder or not args.CleanReadsFolder or not args.PicardFolder  or not args.MergedBamsFolder  or not args.FinalSpeciesName or not args.GATKDirectory: 
@@ -93,21 +94,19 @@ def getListofFastaFilesFromFolders(path):
 def saveToFile(finalCommand, currentStep):
     args = getArgs()
     file = open('commands.sh', 'a')
-    file.write(finalCommand + ' &&' + '\n')
-    if args.log:
-        file.write(finalCommand + +' >> ' + 'SecappopLog/step ' + currentStep + '.txt' + ' &&' + '\n')
+    if args.Log:
+        file.write('{ \n' + finalCommand + '\n}'  + ' 2>&1 | tee ' + 'SecappopLog/' + currentStep + '.txt' + ' && ' + '\n')
     else:
         file.write(finalCommand + ' &&' + '\n')
     file.close()
 
-def gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder):
+def gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder, currentStep):
     newCommand = command.replace('anaconda/GenomeAnalysisTK-3.3-0/GenomeAnalysisTK.jar', GATKDirectory)
-    #newCommand = newCommand.replace('/path/to/4_match-contigs-to-probes', mappingFolder)
     newCommand = newCommand.replace('/path/to/4_match-contigs-to-probes/Genus_species.fasta', ReferenceAssembly)
     newCommand = newCommand.replace('/path/to/7_merge-bams', MergedBamsFolder)
     newCommand = newCommand.replace('Genus_species', FinalSpeciesName)
     finalCommand = newCommand.replace('/path/to/8_GATK',GATKOutputFolder)
-    saveToFile(finalCommand)
+    saveToFile(finalCommand, currentStep)
 
 def step4(sampleIDs, ReferenceAssembly, cleanReadsFolder, mappingFolder):
     command1 = 'bwa index -a is /path/to/4_match-contigs-to-probes/Genus_species.fasta'
@@ -148,7 +147,7 @@ def step5(sampleIDs, MappingFolder):
 
         newCommand1 = command1.replace('/path/to/5-mapping', MappingFolder)
         finalCommand1 = newCommand1.replace('Genus_species', speciesID)
-        saveToFile(finalCommand1)
+        saveToFile(finalCommand1, 'step5')
 
 def step6(sampleIDs, MappingFolder, PicardJarDirectory, PicardFolder):
     command1 = 'java -jar ~/anaconda/jar/CleanSam.jar I=/path/to/5-mapping/Genus_species-aln.bam O=/path/to/6_picard/Genus_species-aln_CL.bam VALIDATION_STRINGENCY=SILENT'
@@ -166,7 +165,7 @@ def step6(sampleIDs, MappingFolder, PicardJarDirectory, PicardFolder):
         newCommand1 = newCommand1.replace('/path/to/5-mapping', MappingFolder)
         newCommand1 = newCommand1.replace('/path/to/6_picard', PicardFolder)
         finalCommand1 = newCommand1.replace('Genus_species', speciesID)
-        saveToFile(finalCommand1)
+        saveToFile(finalCommand1, 'step6')
 
 def step7(sampleIDs, PicardJarDirectory, PicardFolder):
     command1 = 'java -Xmx2g -jar ~/anaconda/jar/AddOrReplaceReadGroups.jar I=/path/to/6_picard/Genus_species-aln_CL.bam  O=/path/to/6_picard/Genus_species-aln_RG.bam SORT_ORDER=coordinate RGPL=illumina RGPU=TestXX RGLB=Lib1 RGID=Genus_species RGSM=Genus_species VALIDATION_STRINGENCY=LENIENT'
@@ -183,7 +182,7 @@ def step7(sampleIDs, PicardJarDirectory, PicardFolder):
         
         newCommand1 = newCommand1.replace('/path/to/6_picard', PicardFolder)
         finalCommand1 = newCommand1.replace('Genus_species', speciesID)
-        saveToFile(finalCommand1)
+        saveToFile(finalCommand1, 'step7')
 
 def step8(sampleIDs, PicardJarDirectory, PicardFolder):
     command1 = 'java -Xmx2g -jar ~/anaconda/jar/MarkDuplicates.jar I=/path/to/6_picard/Genus_species-aln_RG.bam O=/path/to/6_picard/Genus_species-aln_MD.bam METRICS_FILE=/path/to/6_picard/Genus_species.metrics MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=250 ASSUME_SORTED=true REMOVE_DUPLICATES=false'
@@ -201,35 +200,39 @@ def step8(sampleIDs, PicardJarDirectory, PicardFolder):
     
         newCommand1 = newCommand1.replace('/path/to/6_picard', PicardFolder)
         finalCommand1 = newCommand1.replace('Genus_species', speciesID)
-        saveToFile(finalCommand1)
+        saveToFile(finalCommand1, 'step8')
 
 def step9(sampleIDs, PicardJarDirectory, PicardFolder, MergedBamsFolder, FinalSpeciesName):
     commandPart1 =  'java -Xmx2g -jar ~/anaconda/jar/MergeSamFiles.jar SO=coordinate AS=true'
     commandPart2 =  'I=/path/to/6_picard/Genus_species-aln_MD.bam'
     commandPart3 =  'O=/path/to/7_merge-bams/Genus_species.bam'
     args = getArgs()
+    finalCommand1 = ''
 
     if args.PicardJarDirectory:
         if args.SinglePicardJar == True:
             newPicardJarDirectory = PicardJarDirectory + ' MergeSamFiles'  
             finalCommandPart1 = commandPart1.replace('anaconda/jar/MergeSamFiles.jar', newPicardJarDirectory)
-            saveToFile(finalCommandPart1)
+            #saveToFile(finalCommandPart1, 'step9')
+            finalCommand1 = finalCommand1 + finalCommandPart1 + ' '
 
         else:
             finalCommandPart1 = commandPart1.replace('anaconda/jar/MergeSamFiles.jar', PicardJarDirectory)
-            saveToFile(finalCommandPart1)
-
+            #saveToFile(finalCommandPart1, 'step9')
+            finalCommand1 = finalCommand1 + finalCommandPart1 + ' '
 
     for x in sampleIDs:
         speciesID = x
 
         newCommandPart2 = commandPart2.replace('/path/to/6_picard', PicardFolder)
         finalCommandPart2 = newCommandPart2.replace('Genus_species', speciesID) 
-        saveToFile(finalCommandPart2)
+        #saveToFile(finalCommandPart2,'step9')
+        finalCommand1 = finalCommand1 + finalCommandPart2 + ' '
     
     newCommandPart3 = commandPart3.replace('/path/to/7_merge-bams', MergedBamsFolder)
     finalCommandPart3 = newCommandPart3.replace('Genus_species', FinalSpeciesName)
-    saveToFile(finalCommandPart3)
+    finalCommand1 = finalCommand1 + finalCommandPart3 + ' '
+    saveToFile(finalCommand1,'step9')
 
 def step10(MergedBamsFolder, FinalSpeciesName):
     args = getArgs()
@@ -237,7 +240,7 @@ def step10(MergedBamsFolder, FinalSpeciesName):
 
     newCommand1 = command1.replace('7_merge-bams', MergedBamsFolder)
     finalCommand1 = newCommand1.replace('Genus_species', FinalSpeciesName)
-    saveToFile(finalCommand1)
+    saveToFile(finalCommand1,'step10')
 
 
 
@@ -258,7 +261,7 @@ def step11(PicardJarDirectory, mappingFolder, ReferenceAssembly, FinalSpeciesNam
     
     newCommand1 = command1.replace('/path/to/4_match-contigs-to-probes/Genus_species', NewReferenceAssembly)
     finalCommand1 = newCommand1.replace('Genus_species', FinalSpeciesName)
-    saveToFile(finalCommand1)
+    saveToFile(finalCommand1,'step11')
 
 
 def step12(mappingFolder, ReferenceAssembly):
@@ -266,44 +269,44 @@ def step12(mappingFolder, ReferenceAssembly):
     command1 = 'samtools faidx /path/to/4_match-contigs-to-probes/Genus_species.fasta'
 
     finalCommand1 = command1.replace('/path/to/4_match-contigs-to-probes/Genus_species.fasta', ReferenceAssembly)
-    saveToFile(finalCommand1)
+    saveToFile(finalCommand1, 'step12')
 
 def step13(GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder):
     args = getArgs()
     command = 'java -Xmx2g -jar ~/anaconda/GenomeAnalysisTK-3.3-0/GenomeAnalysisTK.jar -T RealignerTargetCreator -R /path/to/4_match-contigs-to-probes/Genus_species.fasta -I /path/to/7_merge-bams/Genus_species.bam  --minReadsAtLocus 7 -o /path/to/8_GATK/Genus_species.intervals'
 
-    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder)
+    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder, 'step13')
 
 
 def step14(GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder):
     args = getArgs()
     command = 'java -Xmx2g -jar ~/anaconda/GenomeAnalysisTK-3.3-0/GenomeAnalysisTK.jar -T IndelRealigner -R /path/to/4_match-contigs-to-probes/Genus_species.fasta -I /path/to/7_merge-bams/Genus_species.bam  -targetIntervals /path/to/8_GATK/Genus_species.intervals -LOD 3.0 -o /path/to/8_GATK/Genus_species_RI.bam'
 
-    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder)
+    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder, 'step14')
 
 def step15(GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder):
     args = getArgs()
     command = 'java -Xmx2g -jar ~/anaconda/GenomeAnalysisTK-3.3-0/GenomeAnalysisTK.jar -T UnifiedGenotyper -R /path/to/4_match-contigs-to-probes/Genus_species.fasta -I /path/to/8_GATK/Genus_species_RI.bam -gt_mode DISCOVERY -o /path/to/8_GATK/Genus_species_raw_SNPs.vcf -ploidy 2 -rf BadCigar'
 
-    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder)
+    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder, 'step15')
 
 def step16(GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder):
     args = getArgs()
     command = 'java -Xmx2g -jar ~/anaconda/GenomeAnalysisTK-3.3-0/GenomeAnalysisTK.jar -T VariantAnnotator -R /path/to/4_match-contigs-to-probes/Genus_species.fasta -I /path/to/8_GATK/Genus_species_RI.bam -G StandardAnnotation -V:variant,VCF /path/to/8_GATK/Genus_species_raw_SNPs.vcf -XA SnpEff -o /path/to/8_GATK/Genus_species_SNPs_annotated.vcf -rf BadCigar'
-
-    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder)
+    
+    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder, 'step16')
 
 def step17(GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder):
     args = getArgs()
     command = 'java -Xmx2g -jar ~/anaconda/GenomeAnalysisTK-3.3-0/GenomeAnalysisTK.jar -T UnifiedGenotyper -R /path/to/4_match-contigs-to-probes/Genus_species.fasta -I /path/to/8_GATK/Genus_species_RI.bam -gt_mode DISCOVERY -glm INDEL -o /path/to/8_GATK/Genus_species_SNPs_indels.vcf -rf BadCigar'
 
-    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder)
+    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder, 'step17')
 
 def step18(GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder):
     args = getArgs()
     command = 'java -Xmx2g -jar ~/anaconda/GenomeAnalysisTK-3.3-0/GenomeAnalysisTK.jar -T VariantFiltration -R /path/to/4_match-contigs-to-probes/Genus_species.fasta -V /path/to/8_GATK/Genus_species_raw_SNPs.vcf --mask /path/to/8_GATK/Genus_species_SNPs_indels.vcf --maskExtension 5 --maskName InDel --clusterWindowSize 10 --filterExpression "MQ0 >= 4 && ((MQ0 / (1.0 * DP)) > 0.1)" --filterName "BadValidation" --filterExpression "QUAL < 30.0" --filterName "LowQual" --filterExpression "QD < 5.0" --filterName "LowVQCBD" -o /path/to/8_GATK/Genus_species_SNPs_no_indels.vcf  -rf BadCigar'
 
-    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder)
+    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder, 'step18')
 
 def step19(FinalSpeciesName, GATKOutputFolder):
     args = getArgs()
@@ -311,10 +314,10 @@ def step19(FinalSpeciesName, GATKOutputFolder):
 
     newCommand1 = command1.replace('/path/to/8_GATK', GATKOutputFolder)
     finalCommand1 = newCommand1.replace('Genus_species', FinalSpeciesName)
-    saveToFile(finalCommand1)
+    saveToFile(finalCommand1, 'step19')
 
 def step20(GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder):
     args = getArgs()
     command = 'java -Xmx2g -jar ~/anaconda/GenomeAnalysisTK-3.3-0/GenomeAnalysisTK.jar -T ReadBackedPhasing -R /path/to/4_match-contigs-to-probes/Genus_species.fasta -I /path/to/8_GATK/Genus_species_RI.bam --variant /path/to/8_GATK/Genus_species_SNPs_pass-only.vcf -L /path/to/8_GATK/Genus_species_SNPs_pass-only.vcf -o /path/to/8_GATK/Genus_species_SNPs_phased.vcf --phaseQualityThresh 20.0 -rf BadCigar'
 
-    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder)
+    gatkCommands(command, GATKDirectory, mappingFolder, ReferenceAssembly, MergedBamsFolder, FinalSpeciesName, GATKOutputFolder, 'step20')
--- GenerateScripts.py
+++ GenerateScripts.py
@@ -1,15 +1,7 @@
 from Functions import *
-import argparse
 
 args = getArgs()
-#fastaFiles = getListofFastaFilesFromFilesargs.CleanReadsFolder)
-#SpeciesID = [x[:-6] for x in fastaFiles]
-
 SpeciesID = getListofFastaFilesFromFolders(args.CleanReadsFolder)
-
-#contigsToProbeFolder = ('Please type your contigs to probe folder: ')
-#cleanReadsFolder = ('Please input the name for a mapping folder: ')
-#mappingFolder = input('Please input the name for a mapping folder: ')
 
 
 if os.path.exists('commands.sh'):
@@ -31,58 +23,217 @@ else:
     file.write('')
     file.close()
 
+if not args.DontMakeFolders:
+    if os.path.exists(args.MappingFolder):
+        if not args.Overwrite:
+            overwrite = input('Mapping Folder already found, do you wish to overwrite? Y/N  ')
+        else:
+            overwrite= 'Y'
 
-if os.path.exists(args.MappingFolder):
-    if not args.Overwrite:
-        overwrite = input('Mapping Folder already found, do you wish to overwrite? Y/N  ')
+        if overwrite == 'Y':
+            os.rmdir(args.MappingFolder)
+            os.makedirs(args.MappingFolder)
+        else:
+            print('Exiting')
+            exit()
     else:
-        overwrite= 'Y'
-
-    if overwrite == 'Y':
-        os.rmdir(args.MappingFolder)
         os.makedirs(args.MappingFolder)
+
+    if os.path.exists(args.PicardFolder):
+        if not args.Overwrite:
+            overwrite = input('Picard Folder already found, do you wish to overwrite? Y/N  ')
+        else:
+            overwrite= 'Y'
+        if overwrite == 'Y':
+            os.rmdir(args.PicardFolder)
+            os.makedirs(args.PicardFolder)
+        else:
+            print('Exiting')
+            exit()
     else:
-        print('Exiting')
-        exit()
-else:
-    os.makedirs(args.MappingFolder)
-
-
-
-
-if os.path.exists(args.PicardFolder):
-    if not args.Overwrite:
-        overwrite = input('Picard Folder already found, do you wish to overwrite? Y/N  ')
-    else:
-        overwrite= 'Y'
-    if overwrite == 'Y':
-        os.rmdir(args.PicardFolder)
         os.makedirs(args.PicardFolder)
-    else:
-        print('Exiting')
-        exit()
-else:
-    os.makedirs(args.PicardFolder)
 
-if os.path.exists(args.MergedBamsFolder):
-    if not args.Overwrite:
-        overwrite = input('Merged bams folder already found, do you wish to overwrite? Y/N  ')
+    if os.path.exists(args.MergedBamsFolder):
+        if not args.Overwrite:
+            overwrite = input('Merged bams folder already found, do you wish to overwrite? Y/N  ')
+        else:
+            overwrite= 'Y'
+        if overwrite == 'Y':
+            os.rmdir(args.MergedBamsFolder)
+            os.makedirs(args.MergedBamsFolder)
+        else:
+            print('Exiting')
+            exit()
     else:
-        overwrite= 'Y'
-    if overwrite == 'Y':
-        os.rmdir(args.MergedBamsFolder)
         os.makedirs(args.MergedBamsFolder)
+
+    if os.path.exists(args.GATKOutputFolder):
+        if not args.Overwrite:
+            overwrite = input('GATK output folder already found, do you wish to overwrite? Y/N  ')
+        else:
+            overwrite= 'Y'
+        if overwrite == 'Y':
+            os.rmdir(args.GATKOutputFolder)
+            os.makedirs(args.GATKOutputFolder)
+        else:
+            print('Exiting')
+            exit()
     else:
-        print('Exiting')
-        exit()
+        os.makedirs(args.GATKOutputFolder)
+
+
+if args.Log:
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step4(SpeciesID, args.ReferenceAssembly, args.CleanReadsFolder , args.MappingFolder)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 4 took $runtime seconds" >> SecappopLog/runtime.txt \n \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step5(SpeciesID, args.MappingFolder)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 5 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step6(SpeciesID, args.MappingFolder, args.PicardJarDirectory, args.PicardFolder)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 6 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step7(SpeciesID, args.PicardJarDirectory, args.PicardFolder)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 7 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step8(SpeciesID, args.PicardJarDirectory, args.PicardFolder)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 8 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step9(SpeciesID, args.PicardJarDirectory, args.PicardFolder, args.MergedBamsFolder, args.FinalSpeciesName)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 9 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step10(args.MergedBamsFolder, args.FinalSpeciesName)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 10 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step11(args.PicardJarDirectory, args.MappingFolder, args.ReferenceAssembly, args.FinalSpeciesName)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 11 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step12(args.MappingFolder, args.ReferenceAssembly)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 12 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step13(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 13 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step14(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 14 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step15(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 15 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step16(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 16 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step17(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 17 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step18(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 18 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step19(args.FinalSpeciesName, args.GATKOutputFolder)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 19 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
+    file = open('commands.sh', 'a')
+    file.write('start=`date +%s.%N`' + '\n')
+    file.close()
+    step20(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
+    file = open('commands.sh', 'a')
+    file.write('end=`date +%s.%N` ' + '\n' + 'runtime=$( echo "$end - $start" | bc -l )' + '\n' + 'echo "Step 20 took $runtime seconds" >> SecappopLog/runtime.txt \n')
+    file.close()
+
 else:
-    os.makedirs(args.MergedBamsFolder)
-
-
-
-step4(SpeciesID, args.ReferenceAssembly, args.CleanReadsFolder , args.MappingFolder)
-step5(SpeciesID, args.MappingFolder)
-step6(SpeciesID, args.MappingFolder, args.PicardJarDirectory, args.PicardFolder)
-step7(SpeciesID, args.PicardJarDirectory, args.PicardFolder)
-step8(SpeciesID, args.PicardJarDirectory, args.PicardFolder)
-step9(SpeciesID, args.PicardJarDirectory, args.PicardFolder, args.MergedBamsFolder, args.FinalSpeciesName)
+    step4(SpeciesID, args.ReferenceAssembly, args.CleanReadsFolder , args.MappingFolder)
+    step5(SpeciesID, args.MappingFolder)
+    step6(SpeciesID, args.MappingFolder, args.PicardJarDirectory, args.PicardFolder)
+    step7(SpeciesID, args.PicardJarDirectory, args.PicardFolder)
+    step8(SpeciesID, args.PicardJarDirectory, args.PicardFolder)
+    step9(SpeciesID, args.PicardJarDirectory, args.PicardFolder, args.MergedBamsFolder, args.FinalSpeciesName)
+    step10(args.MergedBamsFolder, args.FinalSpeciesName)
+    step11(args.PicardJarDirectory, args.MappingFolder, args.ReferenceAssembly, args.FinalSpeciesName)
+    step12(args.MappingFolder, args.ReferenceAssembly)
+    step13(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
+    step14(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
+    step15(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
+    step16(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
+    step17(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
+    step18(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
+    step19(args.FinalSpeciesName, args.GATKOutputFolder)
+    step20(args.GATKDirectory, args.MappingFolder, args.ReferenceAssembly, args.MergedBamsFolder, args.FinalSpeciesName, args.GATKOutputFolder)
